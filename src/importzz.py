26a16a846d2130854727748bd6d6c56f4a5ac4a8
Common <204951789+Commonwealthrocks@users.noreply.github.com>
2025-08-25 01:55:58 +0200

    importzz.py
    All needed imports / libraries for PyLI to work

---
+++ src/importzz.py
@@ -0,0 +1,26 @@
+## importzz.py
+## last updated: 21/8/2025 <d/m/y>
+## p-y-l-i
+
+import warnings
+import sys
+import os
+import json
+import hashlib
+import struct
+import secrets
+import base64
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+warnings.filterwarnings("ignore", category=UserWarning, module="pygame.pkgdata")
+import pygame
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtCore import Signal as pyqtSignal
+from cryptography.exceptions import *
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+## end
